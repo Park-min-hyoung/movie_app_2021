873af32682679bd3e434197ab61cd33334403bf2
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-23 22:30:39 +0900

    장르 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,22 @@ import React from "react";
 import propTypes from "prop-types";
 import "./Movie.css";
 
-function Moive({year, title, summary, poster}) {
-    return <div class="movies__movie">
-        <img src={poster} alt={title} title={title} />
-        <div class="movie_data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+function Moive({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres"> 
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary}</p>
+            </div>
         </div>
-    </div>;
+    );
 }
 
 Moive.prototype = {
@@ -18,7 +25,8 @@ Moive.prototype = {
     year: propTypes.number.isRequired,
     title: propTypes.string.isRequired,
     summary: propTypes.string.isRequired,
-    poster: propTypes.string.isRequired
+    poster: propTypes.string.isRequired,
+    genres: propTypes.arrayOf(propTypes.string).isRequired
 }
 
 export default Moive;
