4334861dcc1ee2d2e410010b2688ef835895d344
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-23 21:35:02 +0900

    Movie.js 만들어 가져온 데이터 출력

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
   };
   getMovie = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: { 
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+    // this.setState({movies:movies}) // 왼쪽 movies는 state, 오른쪽 movies는 data
   }
   componentDidMount() {
     this.getMovie();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div> // 이건 js이다.
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading 
+      ? "Loading" 
+      : movies.map(movie => (
+        <Movie
+          key={movie.id}
+          id={movie.id} 
+          year={movie.year}
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_comver_image} 
+        />
+      ))}
+    </div> 
   }
 }
 
