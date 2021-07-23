d96f059b700f43cc5d6cd474d4974cf5e182ef41
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-23 20:39:52 +0900

    render 함수가 호출 될때 componentDidMount 메소드가 가장 먼저 호출 된다

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-import propTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true
   };
-  add = () => {
-    // this.setState({ count: this.state.count - 1 });
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
-    return (
-    <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div> // 이건 js이다.
   }
 }
 
