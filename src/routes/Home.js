c06ac24025c3784120fb359111157fea78fbf223
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-30 19:46:15 +0900

    프로젝트 완료, 디자인 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,42 +8,43 @@ class Home extends React.Component {
     isLoading: true,
     movies: []
   };
-  getMovie = async () => {
+  getMovies = async () => {
     const {
-      data: { 
+      data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
-    // this.setState({movies:movies}) // 왼쪽 movies는 state, 오른쪽 movies는 data
-  }
+  };
   componentDidMount() {
-    this.getMovie();
+    this.getMovies();
   }
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className="conatiner">
+      <section className="container">
         {isLoading ? (
           <div className="loader">
-            <span className="loaader__text">Loading...</span>
+            <span className="loader__text">Loading...</span>
           </div>
-          ) : (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id} 
-                  year={movie.year}
-                  title={movie.title} 
-                  summary={movie.summary} 
-                  poster={movie.medium_cover_image} 
-                  genres={movie.genres}
-                />
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
             ))}
-            </div>
-          )}
-      </section> 
+          </div>
+        )}
+      </section>
     );
   }
 }
