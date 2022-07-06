652c79d8b6de97a191680552795ec2e68fccbd6d
Park-min-hyoung <qkralswgud1324@naver.com>
2022-07-06 15:56:54 +0900

    UseEffect 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("i run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="입력하세요"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
