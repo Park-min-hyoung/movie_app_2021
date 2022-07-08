1f159b8f9b13dc31a1047ffa7a4293cbba54fade
Park-min-hyoung <qkralswgud1324@naver.com>
2022-07-08 13:10:33 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 // App.js는 router를 render 하는 역할
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
