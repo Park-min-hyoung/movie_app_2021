d6389649b8fada1cb52eb754ed1a5e2b633fbcf2
Park-min-hyoung <qkralswgud1324@naver.com>
2022-07-09 14:18:21 +0900

    Publishing Error solution

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // App.js는 router를 render 하는 역할
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
         <Route path="/hello" element={<h1>Hello</h1>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
